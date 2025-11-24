49446a7dcb43141f3b6602ee4138e4fc29a563db
auriol1601 <auriol1601@gmail.com>
2025-11-24 09:16:10 +0000

    First commit

---
+++ src/bas_java/Main.java
@@ -0,0 +1,17 @@
+package bas_java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int somme = Main.addition(2, 7);
+		System.out.println(somme);
+		
+	}
+	
+	
+	public static int addition(final int entier1, final int entier2) {
+		
+		return entier1 + entier2;
+	}
+
+}
