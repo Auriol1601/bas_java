b52415900fadebf41b5298674d00a455766f7d25
auriol1601 <auriol1601@gmail.com>
2025-11-24 15:07:27 +0000

    feat : class Bloc et instanciation dobjet

--- src/bas_java/Main.java
+++ src/bas_java/Main.java
@@ -1,18 +1,11 @@
 package bas_java;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		List<String> nomsDeslangages = Arrays.asList("Java","PHP","Javascript", "C#");
-		for(String nomDUnLangage : nomsDeslangages)  {
-			String resultat = nomDUnLangage.toUpperCase();
-			System.out.println(resultat);
-		}
-				
-	}	
+		Bloc unBloc = new Bloc(); 
+		unBloc.afficherDescritpion();
+	}
 
 }
 	
