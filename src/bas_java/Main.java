d0a07d5256712bcb8e44976c4b7e40f17b4b0433
auriol1601 <auriol1601@gmail.com>
2025-11-25 15:06:23 +0000

    Nanipulations de l' ensemble des données

--- src/bas_java/Main.java
+++ src/bas_java/Main.java
@@ -1,14 +1,19 @@
 package bas_java;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
-
-	public static void main(String[] args) {
-		Bloc unBlocMur = new Mur(10,10,5,true);
-		//args.afficherBloc();
-		
-		unBlocMur.afficheUneDescription();
-		
-	}
-
+    
+    public static void main(String[] args) {
+        Set<String> motsCles = new LinkedHashSet<String>();
+        motsCles .add("Cabane");
+        motsCles .add("Cabane");
+        motsCles .add("Muraille");
+        
+        for(String motCle : motsCles) {
+            System.out.println(motCle);
+        }
+    }
+    
 }
-	
