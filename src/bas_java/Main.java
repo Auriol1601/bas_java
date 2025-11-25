d9b370f5dbd4e466b9c3ab518acff65d6e48958e
auriol1601 <auriol1601@gmail.com>
2025-11-25 06:47:16 +0000

    Polymorphisme heritage entre classe mere et classe fille

--- src/bas_java/Main.java
+++ src/bas_java/Main.java
@@ -3,8 +3,11 @@ package bas_java;
 public class Main {
 
 	public static void main(String[] args) {
-		Bloc unBloc = new Bloc(); 
-		unBloc.afficherDescritpion();
+		Bloc unBlocMur = new Mur(10,10,5,true);
+		//args.afficherBloc();
+		
+		unBlocMur.afficheUneDescription();
+		
 	}
 
 }
