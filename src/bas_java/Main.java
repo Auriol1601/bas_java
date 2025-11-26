9c230110639a40c7a594c3d47b519d01d68ac13a
auriol1601 <auriol1601@gmail.com>
2025-11-26 02:41:02 +0000

    lOGGING AVEC lOG4J

--- src/bas_java/Main.java
+++ src/bas_java/Main.java
@@ -1,23 +1,16 @@
 package bas_java;
 
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Map;
-
 public class Main {
 
-    public static void main(String[] args) {
-    	
-    	Map<Bloc, Integer> quantiteBloc = new HashMap<Bloc, Integer>();
-    	quantiteBloc.put(new Mur(1,1,1,true),4);
-    	quantiteBloc.put(new Mur(1,1,1,false),4);
-    	
-    	Set<Bloc> cles = quantiteBloc.keySet();
-    	for(Bloc cle : cles) {
-    		Integer valeur = quantiteBloc.get(cle);
-    		System.out.println(cle.getClass().getName() + " : " + valeur);
-    	} 
-    	
-    }
-    
+	public static void main(String[] args) {
+		try {
+			Porte porte = new Porte(1, 1, 1, true);
+			porte.verrouiller();
+		} catch (IllegalBlocException e) {
+			System.out.println("Impossible de construire le bloc.");
+		} catch (PorteVerrouilleException exception) {
+			System.out.println("La porte est déjà vérrouillée.");
+		}
+	}
+
 }
